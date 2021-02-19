23fcf319c506698fac245334c75fd2f4aa350058
Glarix <dancod77@gmail.com>
2021-02-19 14:56:45 +0200

    Added file reading and new record checking

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # TimeCheck
+
+
+1. Taking current user Name
+2. Reading name and score of record holder from file 
+3. Calculating new time 
+4. Checking if record bitten and calling display function
+
--- timetest.c
+++ timetest.c
@@ -4,6 +4,11 @@
 #include <string.h>
 #include <time.h>
 #include <sys/time.h>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
 //Function to print the score board
 void score(double score, char* name){
@@ -24,18 +29,45 @@ void score(double score, char* name){
 
 int main(){
 
+	//opening and reading file with scores   [START]
+	FILE *fptr= fopen("scores.txt", "r");
+	char recordName[5000];
+	char timeScore[5000];
+
+	if (fptr==0){
+        return 1;
+    }
+	fgets(recordName, 5000, fptr);
+	recordName[strlen(recordName) - 1] = '\0';
+	fgets(timeScore, 5000, fptr);
+	double recordTime = atof(timeScore);
+	//opening and reading file with scores [END]
+
+	//getting current player's name [START]
+	char player[256];
+	fgets(player, 257, stdin);
+	player[strlen(player) - 1] = '\0';
+	//[END]
+
+	//Actual time calculation [START]
     struct timeval stop, start;
 	gettimeofday(&start, NULL);
 	//do stuff
 	getchar();
 	gettimeofday(&stop, NULL);
-	
-	char* name = "Uagadougou";
 
 	double acc = (double)((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec) / 1000000;
 	printf("%f seconds\n", acc);
+	//Actual time calculation [END]
 
-	score(acc, name);
+	//Checking if new record and calling display function [START]
+	if (acc > recordTime){
+		acc = recordTime;
+		strcpy(player, recordName);
+	}
+
+	score(acc, player);
+	//Checking if new record and calling display function [END]
 
 
 
